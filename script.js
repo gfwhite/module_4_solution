c1781364c7c8fc70948b407ef052ffc362468fc0
gfwhite <101667505+gfwhite@users.noreply.github.com>
2022-04-28 11:02:06 -0400

    Update script.js
    removed some assignment instructions

--- script.js
+++ script.js
@@ -21,12 +21,6 @@ Hello Paula
 Hello Laura
 Good Bye Jim
 
-WARNING!!! WARNING!!!
-The code does NOT currently work! It is YOUR job to make it work
-as described in the requirements and the steps in order to complete this
-assignment.
-WARNING!!! WARNING!!!
-
 */
 
 // STEP 1:
@@ -58,25 +52,4 @@ for (var i = 0; i < names.length; i++) {
     { helloSpeaker.speak(names[i]) }
     
   }
-//for (/* fill in parts of the 'for' loop to loop over names array */) {
 
-  // STEP 11:
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
-
-  // STEP 12:
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  //if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
-  //} else {
-    // helloSpeaker.xxxx
-  //}
-//}
